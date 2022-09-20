58a326a388c781d86c65477040955a96ecbc6f3e
luisfemojica <fe_mojicar27@hotmail.com>
2022-09-19 23:14:36 -0500

    Primmer Commit

---
+++ useTodos.js
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { useReducer } from "react";
+
+import { todoReducer } from "../08-useReducer";
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+  
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO} Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    const action = {
+      type: "[TODO] Delete Todo",
+      payload: id,
+    };
+
+    dispatch(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    };
+
+    dispatch(action);
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
